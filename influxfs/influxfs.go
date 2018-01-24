9e4b67752ea6f88936f4ad575ad16bc06b6f8412
Lyon Hill <lyondhill@gmail.com>
2018-01-24 14:44:24 -0800

    writing headers complete

--- influxfs/influxfs.go
+++ influxfs/influxfs.go
@@ -4,6 +4,7 @@ import (
 	"errors"
 	"fmt"
 	"os"
+	"time"
 
 	"bazil.org/fuse"
 	"github.com/influxdata/influxdb-client"
@@ -28,6 +29,26 @@ func (fs *FileSystem) Serve(conn *fuse.Conn) error {
 			fmt.Fprintf(os.Stderr, "Error: Could not read request: %s\n", err)
 			continue
 		}
+
+		header := req.Hdr()
+		p := influxdb.Point{
+			Name: "fsevents",
+			Tags: influxdb.Tags{
+				influxdb.Tag{Key: "gid", Value: fmt.Sprintf("%d", header.Gid)},
+				influxdb.Tag{Key: "node", Value: fmt.Sprintf("%d", header.Node)},
+				influxdb.Tag{Key: "pid", Value: fmt.Sprintf("%d", header.Pid)},
+				influxdb.Tag{Key: "uid", Value: fmt.Sprintf("%d", header.Uid)},
+			},
+
+			Fields: map[string]interface{}{
+				"id": header.ID,
+			},
+			Time: time.Now(),
+		}
+		if _, err := p.WriteTo(fs.writer); err != nil {
+			fmt.Fprintln(os.Stderr, err)
+		}
+
 		req.RespondError(errors.New("unimplemented"))
 	}
 	return nil
